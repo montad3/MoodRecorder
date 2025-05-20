c3ef985ad938aeb456254743a20a6ade5b701264
montad3 <montanodaniel199@gmail.com>
2025-05-19 23:23:37 -0500

    Update MoodSelection.py

--- MoodSelection.py
+++ MoodSelection.py
@@ -4,10 +4,13 @@ import plotly.express as px
 from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
+import json
+import os
 
 # --- Google Sheets Setup ---
 SCOPE = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-CREDS = Credentials.from_service_account_file("MoodAccountKey.json", scopes=SCOPE)
+json_str = os.environ.get("GOOGLE_SERVICE_ACCOUNT_JSON")
+CREDS = Credentials.from_service_account_info(json.loads(json_str), scopes=SCOPE)
 client = gspread.authorize(CREDS)
 
 SHEET_NAME = "MoodRecording"
